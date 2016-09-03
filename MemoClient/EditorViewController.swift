871077e30f194555e544d718b0bd637eb65f6212
NUmero <numero@NUmero-no-MacBook-Air.local>
2016-09-03 15:39:26 +0900

    fixed show keyboard

--- MemoClient/EditorViewController.swift
+++ MemoClient/EditorViewController.swift
@@ -3,6 +3,8 @@ import UIKit
 
 class EditorViewController: UIViewController {
 
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    private var originBottomConstraintValue: CGFloat?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -11,6 +13,55 @@ class EditorViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        registerKeyboardNotification()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        unregisterKeyboardNotification()
+    }
+    
+    private func registerKeyboardNotification() {
+        let notificationCenter: NSNotificationCenter = NSNotificationCenter.defaultCenter()
+        
+        notificationCenter.addObserver(self, selector: #selector(EditorViewController.keyboardWillShow), name: UIKeyboardWillShowNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(EditorViewController.keyboardWillHide), name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    private func unregisterKeyboardNotification() {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        let userInfo = notification.userInfo!
+        let keyboardValue = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        let duration : NSTimeInterval = userInfo[UIKeyboardAnimationDurationUserInfoKey]! as! NSTimeInterval
+        
+        if (originBottomConstraintValue == nil) {
+            originBottomConstraintValue = bottomConstraint.constant
+        }
+        
+        bottomConstraint.constant = keyboardValue.height + 8
+        
+        UIView.animateWithDuration(duration, animations: { () -> Void in
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        let userInfo = notification.userInfo!
+        let duration : NSTimeInterval = userInfo[UIKeyboardAnimationDurationUserInfoKey]! as! NSTimeInterval
+        
+        bottomConstraint.constant = originBottomConstraintValue!
+        originBottomConstraintValue = nil
+        
+        UIView.animateWithDuration(duration, animations: { () -> Void in
+            self.view.layoutIfNeeded()
+        })
     }
 
 }
