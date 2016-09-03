53bd1b44720ea9b1998cc36fb5d7671d1f1343da
NUmero <numero@NUmero-no-MacBook-Air.local>
2016-09-03 15:28:50 +0900

    create editor

---
+++ MemoClient/EditorViewController.swift
@@ -0,0 +1,16 @@
+
+import UIKit
+
+class EditorViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
