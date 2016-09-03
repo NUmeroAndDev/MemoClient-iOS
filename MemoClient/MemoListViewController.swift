edf6fc72512e3a31d0364df29f8adee9af1f9f65
NUmero <numero@NUmero-no-MacBook-Air.local>
2016-09-03 15:20:11 +0900

    add refresh button

--- MemoClient/MemoListViewController.swift
+++ MemoClient/MemoListViewController.swift
@@ -77,7 +77,10 @@ class MemoListViewController: UIViewController, UITableViewDelegate, UITableView
     func dispatch_async_global(block: () -> ()) {
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), block)
     }
-
+    
+    @IBAction func reloadItemClicked(){
+        executeLoadMemos()
+    }
 
 }
 
